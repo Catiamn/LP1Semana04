f8a939fd769b8a8ec7e9647e3b5f62b8a602c48b
Cátia Nascimento <catia.nascimento2006@gmail.com>
2025-03-12 15:01:44 +0000

    Fixed some mistakes!

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -6,7 +6,17 @@ namespace BetterDecorador
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine(Decor(args[0], char.Parse(args[1]), int.Parse(args[2])));
+        }
+
+        private static string Decor (string phrase, char symbol, int numbers)
+        {
+            string realsymbol = " ";
+            for (int i = 0; i < numbers; i++)
+            {
+                realsymbol += symbol;
+            }
+            return $"{realsymbol} {phrase} {realsymbol}";
         }
     }
 }
