3d546808779f866b4463778494d2b6b4582d4993
Cátia Nascimento <catia.nascimento2006@gmail.com>
2025-03-12 20:22:35 +0000

    Started adding a text given in case the user fails one of the needed input

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -9,30 +9,32 @@ namespace BetterDecorador
         /// Organizes them with the 
         /// </summary>
         /// <param name="args">The args on the method Decor()</param>
-        private static void Main(string[] args)
-        {
-            Console.WriteLine(Decor(args[0], char.Parse(args[1]), int.Parse(args[2])));
-        }
-        /// <summary>
-        /// Creates a string that keeps adding the character given to the string, so it can later print
-        /// the correct number of times
-        /// </summary>
-        /// <param name="phrase">The phrase given first</param>
-        /// <param name="symbol">The symbol given in second place</param>
-        /// <param name="numbers">Finally the last thing given, the number of time
-        // the symbol will appear </param>
-        /// <returns>The print at the end </returns>
-        private static string Decor (string phrase, char symbol, int numbers)
-        {
-            string realsymbol = " ";
-            for (int i = 0; i < numbers; i++)
+            private static void Main(string[] args)
             {
-                realsymbol += symbol;
+                Console.WriteLine(Decor(args[0], char.Parse(args[1]), int.Parse(args[2])));
             }
-            ///Print at the end
-            ///Prints the Phrase given and the character x number of times
-            /// x is given by the user
-            return $"{realsymbol} {phrase} {realsymbol}";
+            /// <summary>
+            /// Creates a string that keeps adding the character given to the string, so it can later print
+            /// the correct number of times
+            /// </summary>
+            /// <param name="phrase">The phrase given first</param>
+            /// <param name="symbol">The symbol given in second place</param>
+            /// <param name="numbers">Finally the last thing given, the number of time
+            // the symbol will appear </param>
+            /// <returns>The print at the end </returns>
+            private static string Decor (string phrase, char symbol, int numbers)
+            {
+                string realsymbol = " ";
+                for (int i = 0; i < numbers; i++)
+                {
+                    realsymbol += symbol;
+                }
+                ///Print at the end
+                ///Prints the Phrase given and the character x number of times
+                /// x is given by the user
+                return $"{realsymbol} {phrase} {realsymbol}";
+            }
+
+            private static string Decor("User did not specify args !", ’=’, 3)
         }
     }
-}
