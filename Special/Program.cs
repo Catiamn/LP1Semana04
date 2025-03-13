c604d559e9f8c2e717455b6f0760a65dccbb4c49
Cátia Nascimento <catia.nascimento2006@gmail.com>
2025-03-13 14:20:25 +0000

    Added comments

--- Special/Program.cs
+++ Special/Program.cs
@@ -4,6 +4,10 @@ namespace Special
 {
     public class Program
     {
+        /// <summary>
+        /// Ask the user for a number that will later be used
+        /// </summary>
+        /// <param name="args">Only one argument is needed</param>
         private static void Main(string[] args)
         {
             Console.Write("Insert n-th number of the special sequence:");
@@ -11,7 +15,11 @@ namespace Special
             Console.WriteLine(GetSpecial(n));
             
         }
-
+        /// <summary>
+        /// Where the number first given is gonna run, giving an output following a certain equation
+        /// </summary>
+        /// <param name="n">N is the number the user gave</param>
+        /// <returns>The return gives us the final number after all the transformations</returns>
         private static int GetSpecial( int n)
         {
             if (n == 0) return 0;
